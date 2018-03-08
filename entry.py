6cc06947d00965d2fcf980e1c8892f8aa5b68044
liusiming <abanmiku@hotmail.com>
2018-03-08 10:10:28 +0800

    update hk

--- entry.py
+++ entry.py
@@ -21,17 +21,37 @@ logging.config.fileConfig("logger.conf")
 logger = logging.getLogger('example')
 
 
-def crawl(url, code):
+def crawl_us(url, code):
     logger.info('start get {} stock price job'.format(code))
     dcap[
         "phantomjs.page.settings.userAgent"] = \
-        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:51.0) Gecko/20100101 Firefox/51.0"
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36"
     driver = webdriver.PhantomJS(PHANTOMJS_PATH, desired_capabilities=dcap)
     driver.get(url)
     time.sleep(2)
     try:
         logger.info('get {} stock price'.format(code))
         price = float(driver.title.split(' ')[1].split('(')[0])
+        save(code, price)
+        driver.quit()
+    except Exception as e:
+        logger.error(str(e))
+        driver.quit()
+
+
+def crawl_hk(url, code):
+    logger.info('start get {} stock price job'.format(code))
+    dcap[
+        "phantomjs.page.settings.userAgent"] = \
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36"
+    driver = webdriver.PhantomJS(PHANTOMJS_PATH, desired_capabilities=dcap)
+    driver.get(url)
+    time.sleep(2)
+    try:
+        driver.save_screenshot('pic1.png')
+        title = driver.title
+        logger.info('get {} stock price'.format(code))
+        price = float(title.split(' ')[2].split('(')[0])
         save(code, price)
         driver.quit()
     except Exception as e:
@@ -51,7 +71,7 @@ def save(code, price):
         except Exception as e:
             logger.error(str(e))
     else:
-        logger.info('already exists, date is {}'. format(date))
+        logger.info('already exists, date is {}'.format(date))
 
 
 def check_stock_price_exist(code, date):
@@ -66,6 +86,6 @@ def check_stock_price_exist(code, date):
 if __name__ == '__main__':
     logger.info('daily work started')
     for s in us_stock_list:
-        crawl(us_url.format(s), s)
+        crawl_us(us_url.format(s), s)
     for s in hk_stock_list:
-        crawl(hk_url.format(s), s)
+        crawl_hk(hk_url.format(s), s)
